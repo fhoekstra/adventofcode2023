6c14c32f876e285e97dea225b29cb023b8fc287e
Freek Hoekstra <freek.hoekstra@cohesion.nl>
2023-12-07 09:49:52 +0100

    Update test template

--- template/part1/test.py
+++ template/part1/test.py
@@ -3,21 +3,18 @@ from solver import solver
 import unittest
 
 
-TEST_INPUT = '''INPUT'''
+TEST_INPUT = """INPUT"""
 
 EXPECTED = 142
 
 
-def split_into_lines(s: str) -> list[str]:
-    return s.split('\n')
+ITERABLE_INPUT = TEST_INPUT.strip().split("\n")
 
 
 class TestBasicExample(unittest.TestCase):
     def test_basic_example(self):
-        input = split_into_lines(TEST_INPUT)
-        self.assertEqual(EXPECTED, solver(input))
+        self.assertEqual(EXPECTED, solver(ITERABLE_INPUT))
 
 
 if __name__ == "__main__":
     unittest.main()
-
